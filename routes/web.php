e68eeffbb01cc6462ebbcf9e22867e79545daef4
Hadi <hadboard@hotmail.com>
2022-05-12 03:57:57 +0430

    set home route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([], function(){
+    Route::get('/', function () {
+        return view('home');
+    });
 });
+
+
